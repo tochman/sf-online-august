a58a0436e0061fb8f48339f392441287772b369d
Viktoria Blok <viktoria_blok@hotmail.com>
2016-09-27 14:38:43 +0200

    Scaffold the Rails application (#1) (#3)
    Please note that this is being merged into the master branch. 
    * Add user stories
    
    * Scaffold new rails app
    
    * Install development gems
    
    * Install Rspec
    
    * Clean up spec helpers and add shoulda-matchers
    
    * Configure generators in config/application.rb
    
    * Install cucumber
    
    * Generate schema file and db create
    
    * Set up coveralls testing environment
    
    * Add coveralls.wear! to spec_helper
    
    * Remove coverage files from version control
    
    * Remove ActionCable from project
    
    * Remove comments from environment file, refs to Action Cable
    
    * Remove comments in .travis file
    
    * Add back in accidentally deleted bits of database.yml
    
    * Add PR template

---
+++ config/application.rb
@@ -0,0 +1,29 @@
+require_relative 'boot'
+
+require "rails"
+# Pick the frameworks you want:
+require "active_model/railtie"
+require "active_job/railtie"
+require "active_record/railtie"
+require "action_controller/railtie"
+require "action_mailer/railtie"
+require "action_view/railtie"
+require "sprockets/railtie"
+# require "rails/test_unit/railtie"
+
+# Require the gems listed in Gemfile, including any gems
+# you've limited to :test, :development, or :production.
+Bundler.require(*Rails.groups)
+
+module SlowFood
+  class Application < Rails::Application
+    config.generators do |generate|
+      generate.helper false
+      generate.assets false
+      generate.view_specs false
+      generate.helper_specs false
+      generate.routing_specs false
+      generate.controller_specs false
+    end
+  end
+end

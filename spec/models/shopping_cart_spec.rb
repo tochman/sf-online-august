4f8fdf15fde3b700663c0472f537d3c89d5d0237
Amber <amber@amberwilkie.com>
2016-10-06 11:15:20 +0200

    Create has-many association users to shopping carts

--- spec/models/shopping_cart_spec.rb
+++ spec/models/shopping_cart_spec.rb
@@ -3,5 +3,14 @@ require 'rails_helper'
 RSpec.describe ShoppingCart, type: :model do
   describe 'DB table' do
     it { is_expected.to have_db_column :created_at }
+    it { is_expected.to have_db_column :user_id}
+  end
+
+  describe 'dependencies' do
+    it { is_expected.to belong_to :user }
+  end
+
+  describe 'validations' do
+    it { is_expected.to validate_presence_of :user }
   end
 end

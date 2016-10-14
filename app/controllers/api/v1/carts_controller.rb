ac1c03036ed98624631ffc10965221654bf22f4d
tochman <thomas@craftacademy.se>
2016-10-14 10:55:27 +0200

    adds json response to create action

--- app/controllers/api/v1/carts_controller.rb
+++ app/controllers/api/v1/carts_controller.rb
@@ -6,5 +6,6 @@ class Api::V1::CartsController < ApplicationController
     dish = Dish.find(params[:dish_id])
     cart = ShoppingCart.create
     cart.add dish, dish.price
+    render json: ({cart_id: cart.id, dishes: cart.shopping_cart_items})
   end
 end
--- spec/requests/api/v1/create_shopping_cart_spec.rb
+++ spec/requests/api/v1/create_shopping_cart_spec.rb
@@ -8,5 +8,6 @@ RSpec.describe Api::V1::CartsController do
     post '/api/v1/carts', {params: {dish_id: dish.id}}
 
     expect(ShoppingCart.count).to eq 1
+    expect(response_json).to eq({cart_id: 1, dishes: [{dish_id: dish.id}]})
   end
 end

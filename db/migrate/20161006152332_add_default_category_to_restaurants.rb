7dd56f55017a4a545e5772d9d20971a3b0df031f
Raoul DIFFOUO <diraulo@gmail.com>
2016-10-14 09:42:48 +0200

    hotfix migration

--- db/migrate/20161006152332_add_default_category_to_restaurants.rb
+++ db/migrate/20161006152332_add_default_category_to_restaurants.rb
@@ -1,5 +1,5 @@
 class AddDefaultCategoryToRestaurants < ActiveRecord::Migration[5.0]
   def change
-    change_column_default :restaurants, :category, 'Other'
+    change_column :restaurants, :category, default: 'Other'
   end
 end
